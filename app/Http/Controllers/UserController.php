ef47df4c93ac7ce3b322005d70634b90d640861e
Dokpe Wokoma <dokpewokoma@gmail.com>
2021-07-22 09:24:38 +0100

    Initial git push

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+    public function postSignUp(Request $request) {
+        $email = $request['email'];
+        $first_name = $request['first_name'];
+        $last_name = $request['last_name'];
+        $course = $request['course'];
+        $password = bcrypt($request['password']);
+
+        $user = new User();
+        $user->email = $email;
+        $user->first_name = $first_name;
+        $user->last_name = $last_name;
+        $user->course = $course;
+        $user->password = $password;
+
+        $user->save();
+
+        return redirect()->back();
+    }
+
+    public function postSignIn(Request $request) {
+
+    }
+}
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <!-- Image and text -->
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="https://www.mdx.ac.uk/__data/assets/file/0030/548229/logo-mobile.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            mdxHub
+        </a>
+    </nav>
+
+</header>
